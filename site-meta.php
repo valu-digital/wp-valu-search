66cf4ab156adc9bc882a618122d58e0d34668cd2
Esa-Matti Suuronen <esa-matti@suuronen.org>
2019-08-23 15:37:40 +0300

    Use early return in template_redirect

--- site-meta.php
+++ site-meta.php
@@ -4,12 +4,14 @@ namespace ValuSearch;
 
 add_action( 'template_redirect', function(){
 
-	if ( '/valu-search.json' === $_SERVER['REQUEST_URI'] ) {
-		$bloginfo = get_blog_info_array();
-		$config['siteName'] = $bloginfo['blogname'];
-		$content = apply_filters( 'valu_search_site_config' , $config );
-		echo json_encode( $content );
-		die();
-	}
+	if ( '/valu-search.json' !== $_SERVER['REQUEST_URI'] ) {
+        return;
+    }
+
+    $bloginfo = get_blog_info_array();
+    $config['siteName'] = $bloginfo['blogname'];
+    $content = apply_filters( 'valu_search_site_config' , $config );
+    echo json_encode( $content );
+    die();
 
 });
