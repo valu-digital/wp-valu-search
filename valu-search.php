7fac3b3356ff549b255d6efe610e0042bd6d42e4
Esa-Matti Suuronen <esa-matti@suuronen.org>
2019-08-23 14:43:17 +0300

    wp style fixes

--- valu-search.php
+++ valu-search.php
@@ -119,11 +119,11 @@ function get_blog_info_array(){
 
 add_action( 'template_redirect', function(){
 
-	if('/valu-search.json' === $_SERVER['REQUEST_URI']){
+	if ( '/valu-search.json' === $_SERVER['REQUEST_URI'] ) {
 		$bloginfo = get_blog_info_array();
 		$config['siteName'] = $bloginfo['blogname'];
-		$content = apply_filters( 'valu_search_site_config' , $config);
-		echo json_encode($content);
+		$content = apply_filters( 'valu_search_site_config' , $config );
+		echo json_encode( $content );
 		die();
 	}
 
