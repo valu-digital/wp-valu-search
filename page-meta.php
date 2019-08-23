faf1ca542eae64ab93c7be0b328cc3b6e307bd22
Esa-Matti Suuronen <esa-matti@suuronen.org>
2019-08-23 15:35:07 +0300

    Extract page-meta.php and site-meta.php

---
+++ page-meta.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace ValuSearch;
+
+add_action( 'wp_head', function () {
+
+	global $post;
+
+	if ( ! $post ) {
+		return;
+	}
+
+	$public = $post->post_status === 'publish';
+
+	$bloginfo = get_blog_info_array();
+	$blogname = $bloginfo['blogname'];
+	$blog_path = $bloginfo['blog_path'];
+
+	if ( ! $blog_path ) {
+		$blog_path = ROOT_TAG;
+	}
+
+	// Default tags for elasticsearch
+	$tags = [
+		'html',
+		'wordpress',
+		'wp_post_type/' . $post->post_type,
+		'wp_blog_name/' . sanitize_title( $blogname ),
+		'wp_blog_path/' . $blog_path,
+		$public ? 'public' : 'private',
+	];
+
+
+	$meta = [
+		'showInSearch'    => apply_filters( 'valu_search_show_in_search', $post, $public ),
+		'contentSelector' => apply_filters( 'valu_search_content_selector', '.main' ),
+		'cleanupSelector' => apply_filters( 'valu_search_cleanup_selector', '' ),
+		'title'           => $post->post_title,
+		'language'        => substr( get_locale(), 0, 2 ),
+		'created'         => get_the_date( 'c', $post ),
+		'modified'        => get_the_modified_date( 'c', $post ),
+		'tags'            => $tags,
+	];
+
+	// Use the post language if using polylang instead of the blog locale.
+	if ( function_exists( 'pll_get_post_language' ) ) {
+		$meta['language'] = pll_get_post_language( $post->ID, 'slug' );
+	}
+
+	// Allow any custom modifications
+	$meta = apply_filters( 'valu_search_meta', $meta, $post );
+
+	$json = wp_json_encode( $meta );
+	echo "<script type='text/json' id='valu-search'>$json</script>";
+
+} );
--- valu-search.php
+++ valu-search.php
@@ -35,63 +35,6 @@ function can_enable_live_updates() {
 	return true;
 }
 
-if( can_enable_live_updates() ){
-	require('handle-post-change.php');
-}
-
-
-add_action( 'wp_head', function () {
-
-	global $post;
-
-	if ( ! $post ) {
-		return;
-	}
-
-	$public = $post->post_status === 'publish';
-
-	$bloginfo = get_blog_info_array();
-	$blogname = $bloginfo['blogname'];
-	$blog_path = $bloginfo['blog_path'];
-
-	if ( ! $blog_path ) {
-		$blog_path = ROOT_TAG;
-	}
-
-	// Default tags for elasticsearch
-	$tags = [
-		'html',
-		'wordpress',
-		'wp_post_type/' . $post->post_type,
-		'wp_blog_name/' . sanitize_title( $blogname ),
-		'wp_blog_path/' . $blog_path,
-		$public ? 'public' : 'private',
-	];
-
-
-	$meta = [
-		'showInSearch'    => apply_filters( 'valu_search_show_in_search', $post, $public ),
-		'contentSelector' => apply_filters( 'valu_search_content_selector', '.main' ),
-		'cleanupSelector' => apply_filters( 'valu_search_cleanup_selector', '' ),
-		'title'           => $post->post_title,
-		'language'        => substr( get_locale(), 0, 2 ),
-		'created'         => get_the_date( 'c', $post ),
-		'modified'        => get_the_modified_date( 'c', $post ),
-		'tags'            => $tags,
-	];
-
-	// Use the post language if using polylang instead of the blog locale.
-	if ( function_exists( 'pll_get_post_language' ) ) {
-		$meta['language'] = pll_get_post_language( $post->ID, 'slug' );
-	}
-
-	// Allow any custom modifications
-	$meta = apply_filters( 'valu_search_meta', $meta, $post );
-
-	$json = wp_json_encode( $meta );
-	echo "<script type='text/json' id='valu-search'>$json</script>";
-
-} );
 
 function get_blog_info_array(){
 	if ( is_multisite() ) {
@@ -105,15 +48,9 @@ function get_blog_info_array(){
 	return $bloginfo;
 }
 
+require_once( __DIR__ . '/page-meta.php');
+require_once( __DIR__ . '/site-meta.php');
 
-add_action( 'template_redirect', function(){
-
-	if ( '/valu-search.json' === $_SERVER['REQUEST_URI'] ) {
-		$bloginfo = get_blog_info_array();
-		$config['siteName'] = $bloginfo['blogname'];
-		$content = apply_filters( 'valu_search_site_config' , $config );
-		echo json_encode( $content );
-		die();
-	}
-
-});
+if ( can_enable_live_updates() ){
+	require_once( __DIR__ . '/handle-post-change.php');
+}
