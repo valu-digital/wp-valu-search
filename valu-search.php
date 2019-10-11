6c0727b6651fa2a8b61bec49ac8e239e9f4beaa0
Esa-Matti Suuronen <esa-matti@suuronen.org>
2019-10-11 12:48:16 +0300

    Release v0.3.1

--- valu-search.php
+++ valu-search.php
@@ -4,7 +4,7 @@ namespace ValuSearch;
 
 /*
 Plugin Name: Valu Search
-Version: 0.3.0
+Version: 0.3.1
 Plugin URI: https://www.valu.fi
 Description: Expose page metadata for the Search crawler
 Author: Valu Digital
