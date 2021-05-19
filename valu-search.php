e37e30513c6e3e2248d7775e3c65db7890035f15
Joonas Varis <joonas.varis@valu.fi>
2021-05-19 10:39:31 +0300

    Release v0.5.8

--- valu-search.php
+++ valu-search.php
@@ -4,7 +4,7 @@ namespace ValuSearch;
 
 /*
 Plugin Name: Valu Search
-Version: 0.5.7
+Version: 0.5.8
 Plugin URI: https://www.valu.fi
 Description: Expose page metadata for the Search crawler
 Author: Valu Digital
